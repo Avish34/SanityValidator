a661359fd81337b5060d96dc72af603fa5df6611
Avish Porwal <avish@Avishs-MacBook-Pro.local>
2022-12-15 15:31:14 +0530

    added_label_info
    (cherry picked from commit 4146957badfe533959e0ac7151242fe9b9cbf687)

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from Utils.cli import takeUserInput
 from Services.github_service import *
 import sys
 
-pull_request_id = int(takeUserInput())
-print("Thank you for entering your PR id")
-# print("Your PR id is "+pull_request_id)
+def main():
+    pull_request_id = int(takeUserInput())
+    print("Thank you for entering your PR id")
+    # print("Your PR id is "+pull_request_id)
 
-github_service = GithubService(pull_request_id)
-val = github_service.get_latest_commit(pull_request_id)
-print(val)
+    github_service = GithubService(pull_request_id)
+    sha = github_service.get_latest_commit(pull_request_id)
+    pr_info = github_service.get_pr_info(pull_request_id)
+    print(pr_info)
 
+main()
