1fc16101f98b4f818d19a65514add50279a971e7
Avish Porwal <avish@Avishs-MacBook-Pro.local>
2022-12-15 23:29:16 +0530

    Refactor cut 1

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from Services.github_service import *
 
 def find_matching_master_build(github_service, commit_id, dic_of_master_builds):
     while(True):
+        print(commit_id)
         if commit_id in dic_of_master_builds.keys():
             return dic_of_master_builds[commit_id]
         commit_id = github_service.get_parent_commit(commit_id)
@@ -24,21 +25,17 @@ def main():
     print(f"\nParent commit id {sha}\n")
 
     failed_jobs = github_service.get_failed_jobs(sha)
-    #pprint(f"Failed jobs are: {failed_jobs}")
+    # pprint(f"Failed jobs are: {failed_jobs}")
 
     root_job_id = get_root_job_id(failed_jobs)
-    #print(f"\nFetched root job id: {root_job_id}\n")
+    # print(f"\nFetched root job id: {root_job_id}\n")
 
     if root_job_id is not None:
-        target_names = get_target_info(root_job_id)
-        #pprint(f"Fetched target info: {target_names}")
+        pr_root_target = get_failed_root_jobs(root_job_id)
+        pprint(f"Fetched target info: {pr_root_target}")
 
         dic_of_master_builds = get_master_builds()
-        #matching_master_build = find_matching_master_build(github_service, sha, dic_of_master_builds)
-        ### Testing code ###
-        matching_master_build = dic_of_master_builds['74f9ccf2cede4c8dab081a54c980c6936aaa1aea']
-        print(matching_master_build['ID'])
+        matching_master_build = find_matching_master_build(github_service, sha, dic_of_master_builds)
         master_root_target = get_all_jobs_and_targets_info(matching_master_build['ID'])
-        pprint(master_root_target)
-
+        pprint(f"Fetched target info from master build: {master_root_target}")
 main()
