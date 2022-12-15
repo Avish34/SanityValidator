39d49816a91cda0ddee2ea44efb81037e58db0d0
Avish Porwal <avish@Avishs-MacBook-Pro.local>
2022-12-15 21:03:35 +0530

    dev6

--- Utils/jobd_helper.py
+++ Utils/jobd_helper.py
@@ -2,6 +2,8 @@ from pprint import pprint
 
 import requests
 
+master_build_url = "http://jobd.test.pensando.io:3456/submission?release=1&repo=pensando%2Fsw&branch=master&page=0&limit=50"
+
 def get_target_info(job_id):
     """Method to get the target names of the particular job id
 
@@ -16,7 +18,7 @@ def get_target_info(job_id):
 
     json_res = res.json()
     targets = json_res["Targets"]
-    pprint(targets)
+   #pprint(targets)
     target_names = list()
 
     for target in targets:
@@ -29,3 +31,24 @@ def get_target_info(job_id):
         target_names[i]['name'] = target_names[i]['name'].replace('/','%2F')
 
     return target_names
+
+def get_master_builds():
+    res = requests.get(master_build_url)
+    master_builds = res.json()
+    #pprint(json_res)
+    dic_of_master_builds = {}
+    for build in master_builds:
+        sha = build['Jobs'][0]['Ref']['SHA']
+        dic_of_master_builds[sha] = build
+    
+    return dic_of_master_builds
+
+def get_all_jobs_and_targets_info(job_id):
+    master_all_jobs_all_targets_url = f"http://jobd.test.pensando.io:3456/submission/{job_id}"
+    res = requests.get(master_all_jobs_all_targets_url)
+    # print(master_all_jobs_all_targets_url)
+    json_res_all_targets = res.json()
+    for job in json_res_all_targets['Jobs']:
+        if job['SubPath'] == '':
+            return get_target_info(job['ID'])
+    return
--- main.py
+++ main.py
@@ -5,6 +5,15 @@ from pprint import pprint
 from Services.github_service import *
 import sys
 
+def find_matching_master_build(github_service, commit_id, dic_of_master_builds):
+    while(True):
+        if commit_id in dic_of_master_builds.keys():
+            return dic_of_master_builds[commit_id]
+        commit_id = github_service.get_parent_commit(commit_id)        
+    return
+    
+
+
 def main():
     pull_request_id = int(takeUserInput())
     print("Thank you for entering your PR id")
@@ -16,12 +25,20 @@ def main():
     print(f"\nParent commit id {sha}\n")
 
     failed_jobs = github_service.get_failed_jobs(sha)
-    pprint(f"Failed jobs are: {failed_jobs}")
+    #pprint(f"Failed jobs are: {failed_jobs}")
 
     root_job_id = get_root_job_id(failed_jobs)
-    print(f"\nFetched root job id: {root_job_id}\n")
+    #print(f"\nFetched root job id: {root_job_id}\n")
 
     target_names = get_target_info(root_job_id)
-    pprint(f"Fetched target info: {target_names}")
+    #pprint(f"Fetched target info: {target_names}")
+
+    dic_of_master_builds = get_master_builds()
+    #matching_master_build = find_matching_master_build(github_service, sha, dic_of_master_builds)
+    ### Testing code ###
+    matching_master_build = dic_of_master_builds['74f9ccf2cede4c8dab081a54c980c6936aaa1aea']
+    print(matching_master_build['ID'])
+    master_root_target = get_all_jobs_and_targets_info(matching_master_build['ID']) 
+    pprint(master_root_target)
 
 main()
