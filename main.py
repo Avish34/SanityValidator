9eba3a2c3c7c3feed8b1119ba70f79ab5bfa9383
ronakmangal <ronakmangal@pensando.io>
2022-12-15 18:06:43 +0530

    Fetched target names with %2F and its status

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Utils.cli import takeUserInput
 from Utils.common import *
+from Utils.jobd_helper import *
 from pprint import pprint
 from Services.github_service import *
 import sys
@@ -7,16 +8,20 @@ import sys
 def main():
     pull_request_id = int(takeUserInput())
     print("Thank you for entering your PR id")
-    # print("Your PR id is "+pull_request_id)
 
     github_service = GithubService(pull_request_id)
     sha = github_service.get_latest_commit(pull_request_id)
     pr_info = github_service.get_pr_info(pull_request_id)
-    print(pr_info)
-    print("Parent commit id" + sha)
+
+    print(f"\nParent commit id {sha}\n")
+
     failed_jobs = github_service.get_failed_jobs(sha)
-    pprint(failed_jobs)
+    pprint(f"Failed jobs are: {failed_jobs}")
+
     root_job_id = get_root_job_id(failed_jobs)
-    print(root_job_id)
+    print(f"\nFetched root job id: {root_job_id}\n")
+
+    target_names = get_target_info(root_job_id)
+    pprint(f"Fetched target info: {target_names}")
 
 main()
