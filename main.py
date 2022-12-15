ca3926b8b5bed68333e02e551bc09ef841943c6b
Avish Porwal <avish@Avishs-MacBook-Pro.local>
2022-12-15 14:57:45 +0530

    First working code

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from Utils.cli import takeUserInput
+from Services.github_service import *
 import sys
 
-pull_request_id = takeUserInput()
+pull_request_id = int(takeUserInput())
 print("Thank you for entering your PR id")
-print("Your PR id is "+pull_request_id)
+# print("Your PR id is "+pull_request_id)
+
+github_service = GithubService(pull_request_id)
+val = github_service.get_latest_commit(pull_request_id)
+print(val)
 
