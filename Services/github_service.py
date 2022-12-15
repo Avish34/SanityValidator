7f10a7a75194fdceda928eb031ea68da2bf164bf
Avish Porwal <avish@Avishs-MacBook-Pro.local>
2022-12-15 13:21:16 +0530

    class structure

---
+++ Services/github_service.py
@@ -0,0 +1,22 @@
+# Class to serve all the request related to PR and sanity
+class GithubService:
+    def __init__(token, pullRequest, branch):
+        self.token = token
+        self.PR = PR
+
+    def getAllTarget(pullRequest):
+        # check whether the PR is valid or not, add try catch
+        verifyPullRequest(pullRequest)
+        # Use API
+
+    def getLatestCommit(pullRequest):
+        # Use API
+
+    def getParentCommit(sha);
+        # Use API
+
+    def verifyPullRequest(pullRequest):    
+        # Use API
+
+    def getPullRequest(sha):
+        # Use API    
