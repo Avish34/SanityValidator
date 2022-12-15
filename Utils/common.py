0a032bb17b33683ebf1b1b485701e57ae0530f96
nikhilsk <47417007+nikhilsk@users.noreply.github.com>
2022-12-15 23:55:59 +0530

    Convert_target_dictionary (#16)

--- Utils/common.py
+++ Utils/common.py
@@ -9,3 +9,12 @@ def get_root_job_id(jobs):
     for job in jobs:
         if job['context'] == '*root*':
             return parse_job_id(job['description'])
+
+def parse_targets(target_name):
+    res = target_name.split("%2F",1)
+    return res[1]
+
+def convert_target_dictionary(target_list):
+    for target_dictionary in target_list:
+        target_dictionary['name'] = parse_targets(target_dictionary['name'])
+    return target_list
